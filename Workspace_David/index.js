ef1e58bcf0c534be44749d5b4574d32622f203c0
DavidGonz <davidgramiro@gmail.com>
2022-10-11 18:56:06 +0200

    Inputs for form done

--- Workspace_David/index.js
+++ Workspace_David/index.js
@@ -21,12 +21,13 @@ let box = document.createElement("input")
 box.id = "dni"
 box.type = "text"
 box.placeholder = "Tu DNI"
+box.required
 contenedor.appendChild(box)
 
 // Campo Nombre
 let label2 = document.createElement("label")
 label.id = "label_nom"
-let nom = document.createTextNode("Nombre y Apellidos")
+let nom = document.createTextNode("Nombre y apellidos")
 label2.appendChild(nom)
 contenedor.appendChild(label2)
 
@@ -37,9 +38,35 @@ box2.placeholder = "Tu nombre y apellidos"
 contenedor.appendChild(box2)
 
 // Campo dirección
+let label3 = document.createElement("label")
+label.id = "label_dir"
+let address = document.createTextNode("Dirección")
+label3.appendChild(address)
+contenedor.appendChild(label3)
 
+let box3 = document.createElement("input")
+box3.id = "dir"
+box3.type = "text"
+box3.placeholder = "Escribe tu dirección"
+contenedor.appendChild(box3)
 
 // Campo telf
+let label4 = document.createElement("label")
+label4.id = "label_telf"
+let telefono = document.createTextNode("Teléfono")
+label4.appendChild(telefono)
+contenedor.appendChild(label4)
+
+let box4 = document.createElement("input")
+box4.id = "telf"
+box.type = "number"
+contenedor.appendChild(box4)
+
+//Radio buttons
+
+
+
+
 
 
 
