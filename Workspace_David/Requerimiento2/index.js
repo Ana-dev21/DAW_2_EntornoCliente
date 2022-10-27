d6a7c339495bd54646e533be1604ceb7614ca1bb
DavidGonz <davidgramiro@gmail.com>
2022-10-27 20:46:10 +0200

    falta rematar

--- Workspace_David/Requerimiento2/index.js
+++ Workspace_David/Requerimiento2/index.js
@@ -13,11 +13,43 @@ function totalTamaños(){
         alert("Ninguna seleccionada")
     }
     let euros = parseInt(x)
-    return alert(`El precio es ${euros}`)
+    return euros
 }
+
+function totalIngredientes(){
+    const valor1 = document.getElementById("bacon");
+    const valor2 = document.getElementById("champiñones");
+    const valor3 = document.getElementById("piña");
+    const valor4 = document.getElementById("pulledPork")
+
+
+    //No me gusta nada este método, pero mientra tanto............. ;(
+    let y = 0;
+    if (valor1.checked == true) {
+      y++; 
+    }
+    if (valor2.checked == true) {
+      y++;
+    }
+    if (valor3.checked == true) {
+      y++;
+    }
+    if (valor4.cheked == true){
+        y++;
+    }
+    let euros = parseInt(y)
+    return euros;
+}
+
+function precioTotal(){
+
+    return alert("El precio es  " + `${totalTamaños()}` + `${totalIngredientes()}` +"Euros")
+}
+
 
 window.onload = function(){
 
-    let envio = document.getElementById("finalizar")
-    envio.addEventListener("click", totalTamaños)
+    let tamañoPizza = document.getElementById("finalizar")
+    tamañoPizza.addEventListener("click", precioTotal)
+
 }
